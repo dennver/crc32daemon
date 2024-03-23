ee00fea4415537412fb20ef35cdd5a83f09f452d
dennver <dennver12@gmail.com>
2024-03-23 13:25:59 +0300

    add signal handler stub

--- app/main.cpp
+++ app/main.cpp
@@ -1,4 +1,23 @@
+
+#include <csignal>
+
+
+// gracefull shutdown
+void signal_handler(int signum)
+{
+   exit(signum);  
+}
+
 int main(int argc, char *argv[])
 {
+    // Install a signal handler
+    if (std::signal(SIGTERM, signal_handler) == SIG_ERR)  
+       return -1;
+    std::signal(SIGQUIT, SIG_IGN);
+    std::signal(SIGINT, SIG_IGN);
+    std::signal(SIGHUP, SIG_IGN);
+    std::signal(SIGSTOP, SIG_IGN);
+    std::signal(SIGCONT, SIG_IGN);
+
     return 0;
 }
