8a1feda5b2488de30273d91614280141de223ff3
dennver <dennver12@gmail.com>
2024-03-29 10:51:36 +0300

    fix cmakelists & main stub

--- app/main.cpp
+++ app/main.cpp
@@ -1,18 +1,103 @@
-#include <iostream>
-#include <getopt.h>
-
+#include <chrono>
 #include <csignal>
+#include <signal.h>
+#include <sys/stat.h>
+#include <thread>
 
+#include "checksum_worker.h"
+#include "crc32.h"
+#include "event_queue.h"
+#include "notifier.h"
+#include "params_parser.h"
+#include "syslogger.h"
+#include "timeout_worker.h"
 
-// gracefull shutdown
-void signal_handler(int signum)
+volatile sig_atomic_t do_shutdown = 0;
+// gracefull shutdown 
+void signal_handler(int status)
 {
-   exit(signum);  
+    do_shutdown = status;
 }
 
-int main(int argc, char *argv[])
-{
-    // Install a signal handler
+void signalHandler(int signal) {
+    switch(signal) {
+        case SIGHUP:
+            // Handle SIGHUP signal
+            break;
+        case SIGTERM:
+            // Handle SIGTERM signal
+            exit(0);
+            break;
+    }
+}
+
+int daemonize() {
+    // Fork off the parent process
+    pid_t pid = fork();
+    if (pid < 0) {
+        exit(EXIT_FAILURE);
+    }
+    if (pid > 0) {
+        exit(EXIT_SUCCESS);
+    }
+
+    // Change the file mode mask
+    // Since the child process is a daemon, the umask needs to be set so files and logs can be written
+    umask(0);
+
+    // Create a new SID for the child process
+    pid_t sid = setsid();
+    if (sid < 0) {
+        // Log failure and exit
+        syslog(LOG_ERR, "Could not generate session ID for child process");
+        exit(EXIT_FAILURE);
+    }
+
+    // Close standard streams
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+
+    return 0;
+}
+
+
+int main(int argc, char *argv[]) {
+    std::shared_ptr<SyslogWriter> logger(new SyslogWriter(),  [](SyslogWriter* writer)
+        {
+            RemoveLogger();
+            delete writer;
+        });
+    SetLogger(logger);
+    
+    using namespace CommandLine;
+    CommandLineArgsParser::Option opt1{CommandLineArgsParser::ValueType::STRING};
+    CommandLineArgsParser::Option opt2{CommandLineArgsParser::ValueType::INT};
+    CommandLineArgsParser::Option opt3{CommandLineArgsParser::ValueType::NOVAL};
+    std::unordered_map<std::string, CommandLine::CommandLineArgsParser::Option> options{
+        {"--directory", opt1},
+        {"--timeout", opt2},
+        // {"--debug", opt3}
+    };
+    CommandLineArgsParser parser(options);
+
+    try
+    {
+        parser.Parse(argc, (const char**)argv);
+    }
+    catch(const std::exception& e)
+    {
+        logger->writeLog(e.what());
+        return -1;
+    }
+
+    std::string directory = parser.GetValue<std::string>("--directory");
+    int timeout = parser.GetValue<uint32_t>("--timeout");
+
+    // Start the daemon
+    daemonize();
+
+    // Set signal handlers
     if (std::signal(SIGTERM, signal_handler) == SIG_ERR)  
        return -1;
     std::signal(SIGQUIT, SIG_IGN);
@@ -50,5 +135,6 @@ int main(int argc, char *argv[])
             return 1;
         }
     }
+    
     return 0;
 }
