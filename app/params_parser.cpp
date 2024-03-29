e827a41971b5c52d3808e843bcac718fe3f3449d
dennver <dennver12@gmail.com>
2024-03-29 16:02:05 +0300

    code to deat with

--- app/params_parser.cpp
+++ app/params_parser.cpp
@@ -4,6 +4,58 @@
 
 #include "params_parser.h"
 
+/*CmdlineParser::CmdlineParser(int argc, char *argv[], const CmdParamsList& params)
+{
+    // const CmdParamsList& params) //: Options(params.size())
+
+    std::vector<option> options;
+    options.reserve(params.size());
+    std::transform(params.cbegin(), params.cend(), std::back_inserter(options), [](const Option& opt)
+    {
+        return option{opt.GetKey().c_str(), opt.GetHasArg(), 0, opt.GetShortKey()};
+    });
+    options.push_back({0, 0, 0, 0});
+
+
+    std::map<std::string, OptionValue> retVal;
+
+    int opt   = 0;
+    int index = 0;
+
+    char str[params.size()*2 - 1];
+    for (int i = 0; i < params.size(); ++i)
+    {
+        str[i++] = options.at(i).val;
+        if (i%2 == 0)
+            str[i++] = ':';
+    }
+
+    while ((opt = getopt_long(Argc, Argv, str, options.data(), &index)) != -1) {
+    //         switch(opt) {
+    //         // -d, --directory <directory name> dir path
+    //         case 'd':
+    //         {
+    //             std::cout << optarg << std::endl;
+    //             dir = optarg;
+    //             params.insert({'d', optarg});
+    //         }
+    //             break;
+    //         case 'v':
+
+    //             std::cout << "Version: " << std::endl;
+    //             params.insert({'d', optarg});
+
+    //             return 0;
+    //         case 0: // long option without a short arg
+    //             break;
+    //         default:
+    //             std::cout << "Incorrect prompt parameters";
+    //             return 1;
+    //         }
+        }
+}
+*/
+
 std::string GetShortOpts(int len, const std::vector<option>& options)
 {
     std::string shortOpts;
